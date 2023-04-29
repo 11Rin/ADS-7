af243149361301e4770720c7b3a0b0d2f629ea4a
Irina <116427830+11Rin@users.noreply.github.com>
2023-04-29 21:21:06 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,12 +1,61 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
+#include <string>
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
-};
+ private:
+  struct QItem {
+    T data;
+    QItem * next;
+    QItem * prev;
+  };
+  QItem * head, * tail;
+  TPQueue::QItem * create(const T& value) {
+    QItem * item = new QItem;
+    item -> data = value;
+    item -> next = item -> prev = nullptr;
+    return item;
+  }
 
+ public:
+  T pop() {
+    if (head == nullptr)
+      throw std::string("Empty");
+    else {
+      QItem * temp = head -> next;
+      if (temp)
+        temp -> prev = nullptr;
+      T data = head -> data;
+      delete head;
+      head = temp;
+      return data;
+    }
+  }
+  void push(const T& value) {
+    QItem * temp = head;
+    QItem * item = create(value);
+    while (temp && temp -> data.prior >= value.prior)
+      temp = temp -> next;
+    if (!temp && head) {
+      tail -> next = item;
+      tail -> next -> prev = tail;
+      tail = item;
+    } else if (!temp && !head) {
+        head = tail = item;
+    } else if (!temp -> prev) {
+        temp -> prev = item;
+        item -> next = temp;
+        head = item;
+    } else {
+        temp -> prev -> next = item;
+        item -> prev = temp -> prev;
+        item -> next = temp;
+        temp -> prev = item;
+      }
+    }
+};
 struct SYM {
   char ch;
   int prior;
